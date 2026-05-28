62dae33134b98342db58dd17bdb7cf3b938b1139
Brendan Liang <liang.bren@gmail.com>
2026-05-28 18:51:13 +1000

    Added basic maix code (UART + test case)

---
+++ maix/main.py
@@ -0,0 +1,15 @@
+from camera import Camera
+import time
+
+cam = Camera(720, 480)
+running = True
+
+start_time = time.time()
+
+cam.send_packet(1, -120)
+
+time.sleep(2)
+if cam.uart.is_open():
+    cam.uart.close()
+print("exiting...")
+time.sleep(3)
