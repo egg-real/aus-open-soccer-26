efce95229e8e7693f1c3b9adf902ce43912a91f2
Brendan Liang <liang.bren@gmail.com>
2026-05-30 22:32:35 +1000

    Working simple ball detect  - Restructed camera.py (cv2 now works)  - Simple ball detection in main.py  - Created maix_setup.md to guide WiFi setup  - Update .gitignore for MaixCam configs

--- maix/camera.py
+++ maix/camera.py
@@ -1,10 +1,47 @@
-from maix import camera as _camera, uart as _uart, pinmap as _pinmap
-import time
+from maix import camera as _camera, uart as _uart, pinmap as _pinmap, image as _image, display as _display, time as _time
+from math import sin, cos, pi
 
 
-class Camera(_camera.Camera):
-    def __init__(self, w:int, h:int, port="/dev/ttyS3"):
-        super().__init__(w, h)
+class Camera():
+    def __init__(self, w:int, h:int, debug=False, show=False):
+        self.cam = _camera.Camera(w, h)
+        self._debug = debug
+        self._show = show
+
+        self._w = w
+        self._h = h
+
+        if debug:
+            self.disp = _display.Display()
+
+    def read(self, prev_angle=None):
+        try:
+            img = self.cam.read()
+            frame = _image.image2cv(img)
+            
+            if self._show:
+                if prev_angle is not None:
+                    a = -(prev_angle - 90) * (pi/180)
+                    length = 50
+                    cx = int(self._w / 2)
+                    cy = int(self._h - 1)
+                    end_x = int(cx + cos(a) * length)
+                    end_y = int(cy - sin(a) * length)
+
+                    img = img.draw_line(cx, cy, end_x, end_y, _image.COLOR_ORANGE, 2)
+                self.disp.show(img)
+
+            if self._debug:
+                print(f"FPS: {_time.fps()}")
+
+            del img
+            return True, frame
+        except RuntimeError:
+            return False, None
+
+
+class UART():
+    def __init__(self, port="/dev/ttyS3"):
         # CHECK THAT PINS ARE MAPPED BEFORE PORT IS INITIALISED
         _pinmap.set_pin_function("P19", "UART3_TX")
         _pinmap.set_pin_function("P20", "UART3_RX")
@@ -12,30 +49,24 @@ class Camera(_camera.Camera):
         self.uart = _uart.UART(port, 115200)
 
         print("\n\n!!!PORT!!!: " + self.uart.get_port() + "\n\n")
-
-    def _write(self, msg=0b0100010):
+    
+    def _write(self, msg=0b00000000.to_bytes(1)):
         if not self.uart.is_open():
             return False
         
-        print("[debug] wrote " + bin(msg))
+        print("[debug] wrote " + bin(int.from_bytes(msg)))
         self.uart.write(msg)
-        time.sleep(0.005)
+        _time.sleep_ms(5)
         
         return True
     
-    def send_packet(self, has_ball:bool=1, ball_dir:int=128):
+    def send_packet(self, has_ball:bool=True, ball_dir:int=128):
         packet = 0b0
         
         sign_bit = bool((ball_dir/abs(ball_dir) + 1) / 2)
-        num = min(abs(ball_dir), 127)
-        packet = ((packet + sign_bit) << 7) + num
+        num = min(abs(int(ball_dir)), 127)
+        packet = ((packet + sign_bit) << 7) | num
 
         # packet = (packet << 1) + has_ball
 
-        self._write(packet)
-
-
-        
-if __name__ == "__main__":
-    cam = Camera(360, 540)
-    frame = cam.read()
+        self._write(packet.to_bytes((packet.bit_length() + 7) // 8))
--- maix/main.py
+++ maix/main.py
@@ -1,15 +1,81 @@
-from camera import Camera
+import cv2
+from math import tan, pow, pi, cos, sin, atan2
+import numpy as np
 import time
 
-cam = Camera(720, 480)
+from camera import Camera, UART
+
+fieldW, fieldL = 1820, 2430
+fieldRW, fieldRL = int(fieldW / 2), int(fieldL / 2) 
+HFOV = 81
+screenToWorldCartesian = None
+screenToWorldPolar = None
+
+#screenToWorldPolar[screenx, screeny] -> (distance, angle)
+#screenToWorldCartesian[worldx, worldy] -> (x, y)
+
+# with open("screen2world.txt",'r') as file:
+#     s2wc, s2wp = file.read().strip().split(sep = "\ndelimiter\n")
+#     screenToWorldCartesian = np.array(eval(s2wc))
+#     screenToWorldPolar = np.array(eval(s2wp))
+cameraZ = 158 
+cameraAOD = pi/6
+div = 374.67 
+def getAngle(xPixel, yPixel):
+    z = ((179.5 - yPixel) / div) * cos(cameraAOD) - sin(cameraAOD)
+    y = cos(cameraAOD) + ((179.5 - yPixel) / div) * sin(cameraAOD)
+    x = (xPixel-319.5) / div
+    pos = np.array([x,y,z])
+    pos /= np.linalg.norm(pos)
+    return atan2(*(pos * cameraZ / -pos[2])[:2]) * (180/pi)
+
+def getBoundingPolygon(mask):
+    ct, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    if len(ct) == 0:
+        return []
+    return max(ct,key=cv2.contourArea)
+
+
+paths = ["origin","-500 -300","800 -1000","100 900"]
+colourSpecs = {
+    # colour : ( HSV , HSV Weighting , lowerbound , upperBound)
+    "org" : np.array(([8,200,200],[18,256,256])),
+    "blu" : np.array(([100,255,255],[100,255,255])),
+    "ylw" : np.array(([19,200,180],[25,255,255])),
+    "grn" : np.array(([40,65,75],[70,155,180])),
+    "blk" : np.array(((0,0,0),(180,20,20)))
+}
+
+cap = Camera(640, 360, debug=True, show=False)
+uart = UART()
+
 running = True
 
-start_time = time.time()
+hsvFrame = None
+k = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
 
-cam.send_packet(1, -120)
+prev_angle = 0
 
-time.sleep(2)
-if cam.uart.is_open():
-    cam.uart.close()
-print("exiting...")
-time.sleep(3)
+try:
+    while running:
+        ret, frame = cap.read(prev_angle)
+        if not ret:
+            continue
+        hsvFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        ballMask = cv2.inRange(hsvFrame,*colourSpecs["org"])
+        ct = getBoundingPolygon(ballMask)
+        if len(ct) > 0:
+            (x,y), r = cv2.minEnclosingCircle(ct[0])
+            angle = getAngle(x, y)
+            prev_angle = angle
+            print("angle:", angle)
+            uart.send_packet(1, angle)
+        else:
+            print("no balls")
+except KeyboardInterrupt:
+    running = False
+finally:
+    time.sleep(1)
+    if uart.uart.is_open():
+        uart.uart.close()
+    time.sleep(2)
