bdfcb6e98ec10baceb976a7720f8f5a5f3e35292
Arhan Busam <arhan.busam@gmail.com>
2026-06-03 10:31:44 +1000

    Add push_pull_forward.py

---
+++ pi/push_ball_forward.py
@@ -0,0 +1,30 @@
+import math
+from camera import Cameras
+
+cams = Cameras()
+
+while True:
+    cams.process()
+    ball_dir = cams.get_ball_dir()
+    ball_dist = cams.get_ball_dist()
+    if ball_dir is not None and ball_dist is not None:
+        print(f"Ball direction: {ball_dir}, distance: {ball_dist}")
+        dx = ball_dist * math.cos(ball_dir)
+        dy = ball_dist * math.sin(ball_dir)
+        direction = math.degrees(math.atan2(dy, dx))
+
+        offset = 0
+        speed = 0.5
+        if ball_dist < 400:
+            if -10 < direction < 10:
+                direction = 0
+                speed = 0.8
+            elif 0 < direction < 180:
+                offset = 80
+            else:
+                offset = -80
+        elif ball_dist > 500:
+            speed = 800
+        direction += offset
+    else:
+        print("Ball not found")
