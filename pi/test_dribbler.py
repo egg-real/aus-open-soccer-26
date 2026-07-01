5d0adb90ce1de79ba896821c33f5f4ceae60b67d
Brendan Liang <liang.bren@gmail.com>
2026-07-01 15:32:32 +1000

    Fix test_dribbler direction

--- pi/test_dribbler.py
+++ pi/test_dribbler.py
@@ -5,7 +5,7 @@ from lib.dribbler import Dribbler
 if __name__ == "__main__":
     dribbler = Dribbler()
     try:
-        dribbler.set_speed(-1.0)
+        dribbler.set_speed(1.0)
         time.sleep(5)
     finally:
         dribbler.set_speed(0)
